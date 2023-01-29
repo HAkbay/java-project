4c0fd5549f5282324c40e02efc7738573b80910e
CreeplyCreep <creeplycreep@gmail.com>
2023-01-29 22:09:47 +0300

    Minimal changes
    Removed some comments, edited file location name

--- javaProject/javaProject/mainClass.java
+++ javaProject/javaProject/mainClass.java
@@ -3,9 +3,9 @@ import java.io.File;
 
 public class mainClass {
 	//Path of the main file you will be using to remove comments/ identify loop, methods and constructs
-	public static File mainFile = new File("C:\\Users\\Haluk\\eclipse-workspace\\schoolProject\\schoolProject\\identifyLoopsMethods.java"); //i used identify loops java file as input
+	public static File mainFile = new File("C:\\Users\\Haluk\\eclipse-workspace\\myProject\\myProject\\exampleFile.java");
 	//Path of the file which will be the output as the result of our code
-	public static File writtenFile = new File("C:\\Users\\Haluk\\eclipse-workspace\\schoolProject\\schoolProject\\writtenfile.txt"); //writtenfile.txt is included in schoolProject file
+	public static File writtenFile = new File("C:\\Users\\Haluk\\eclipse-workspace\\myProject\\myProject\\outputFile.txt");
 	
 	public static void main(String[] args) {
 		//Removing comments and writing onto the file you will be using as output file
